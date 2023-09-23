af5e10b8349b2786d3f547d80ff31e3656cf7200
Brian Tsalwa <brian.tsalwa@moringaschool.com>
2023-09-23 15:40:31 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,53 @@
+function add(a, b){
+  return a + b;
+}
+
+add(69,25);
+
+function subtract(a, b){
+  return a - b;
+}
+
+subtract(60, 54);
+
+function multiply(a, b){
+  return a * b;
+}
+
+multiply(890, 2);
+
+function divide(a, b){
+  return a/ b;
+}
+
+add(56, 7);
+
+// NUmber increments
+
+let n = 5;
+
+function increment(n){
+  n++;
+  return n;
+}
+
+
+n = 150;
+
+function decrement(n){
+  --n;
+  return n;
+}
+
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+
+//preserveDecimal(n)
+     //preserves n's decimals (it parses n as a floating point number) and returns the parsed number:
+
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}       
